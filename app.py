ae0364186e40dfb6d1174bc8bee7acf93e42770d
Khrystyna Peniak <khrystyna.peniak.22@pnu.edu.ua>
2024-10-15 18:00:43 +0300

    Class-work: added griting function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +8,15 @@ def main():
 @app.route('/homepage') 
 def home():
     """View for the Home page of your website."""
-    return f"This is your homepage :) "
+    agent = request.user_agent
+    return f"<h1>This is your homepage :) - {agent}</h1>"
+
+@app.route('/hi/<string:name>')  
+def greeting(name):
+    name = name.upper()
+    age = request.args.get("age", 0, type=int)
+    year = 2024 - age
+    return f"Welcome {name=} {age=}"
 
 if __name__ == '__main__':
     app.run(debug=True)
