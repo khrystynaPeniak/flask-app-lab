a479b0cc35eaeffe013c071880fdca5d01a56c8d
Khrystyna Peniak <khrystyna.peniak.22@pnu.edu.ua>
2024-10-30 17:22:11 +0300

    Сlass-work: add cookies

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
 from . import user_bp
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, make_response
+from datetime import timedelta, datetime
 
 #users
 
@@ -15,3 +16,21 @@ def admin():
     to_url = url_for("users.greetings", name="administrator", age=45, _external=True)   
     print(to_url)
     return redirect(to_url)  
+
+@user_bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука встановлена')
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    response.set_cookie('color', 'black', max_age=timedelta(seconds=60))
+    return response
+
+@user_bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+@user_bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    return response
