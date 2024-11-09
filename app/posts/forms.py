427f1f894412c8ab81f8a7b8bdc799f3cfc56d91
Khrystyna Peniak <khrystyna.peniak.22@pnu.edu.ua>
2024-11-09 22:41:35 +0300

    Lab5: add wtf form for post and update base.html

---
+++ app/posts/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class PostFrom(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(min=2, max=10)])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Add Post")
