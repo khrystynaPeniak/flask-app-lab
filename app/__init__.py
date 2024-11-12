ea456522b5cf4cb233ab9802484ec7e0aaca8388
Khrystyna Peniak <khrystyna.peniak.22@pnu.edu.ua>
2024-11-12 23:57:41 +0300

    Lab6: refactor posts/views.py and add utils.py, static/images, factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 
+def create_app(config_name="config"):
+    app = Flask(__name__)
+    app.config.from_object(config_name)  # налаштування з об'єкта
 
-app = Flask(__name__)
-app.config.from_pyfile("../config.py")
+    with app.app_context():
+        from . import views
 
-from . import views
+        from .posts import post_bp
+        from .users import user_bp
 
-from .posts import post_bp
-app.register_blueprint(post_bp)
+        app.register_blueprint(post_bp)
+        app.register_blueprint(user_bp)
 
-from app.users import user_bp
-app.register_blueprint(user_bp)
+    return app
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,41 +1,25 @@
-import datetime
 from . import post_bp
-from flask import render_template, abort, flash, url_for, redirect, session
+from flask import render_template, abort, flash, url_for, redirect, session, request
 from .forms import PostFrom
-import json
-
-def load_posts():
-    try:
-        with open('app/posts.json', 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
-
-def save_posts(posts):
-    with open('app/posts.json', 'w', encoding='utf-8') as f:
-        json.dump(posts, f, ensure_ascii=False, indent=4, default=str)
-
+from .utils import load_posts, save_post, get_post
 
 @post_bp.route('/add_post', methods=['GET', 'POST'])
 def add_post():
     form = PostFrom()
     if form.validate_on_submit():
-        posts = load_posts()
-        new_post = {
-            "id": len(posts) + 1,
-            "title": form.title.data,
-            "content": form.content.data,
-            "is_active": form.is_active.data,
-            "publish_date": form.publish_date.data.strftime('%Y-%m-%d'),
-            "category": form.category.data,
-            "author": session.get('username'),
-        }
-        posts.append(new_post)
-        save_posts(posts)
-        
-        flash(f"Post '{form.title.data}' added successfully!", "success")
+        title = form.title.data
+        content = form.content.data
+        publish_date = form.publish_date.data.strftime('%d.%m.%Y')
+        category = form.category.data
+        is_active = form.is_active.data
+        author = session.get('username')
+        new_post = {"id": len(load_posts()) + 1, 'title': title, 'content': content, 'publish_date': publish_date,
+                    'category': category, 'author': author, 'is_active': is_active}
+        save_post(new_post)
+        flash(f"Post {title} added successfully!", "success")
         return redirect(url_for(".get_posts"))
-        
+    elif request.method == "POST":
+        flash(f"Enter the correct data in the form!", "danger")
     return render_template("add_post.html", form=form)
     
 
@@ -47,8 +31,7 @@ def get_posts():
 
 @post_bp.route('/<int:id>') 
 def detail_post(id):
-    posts = load_posts()
-    post = next((post for post in posts if post['id'] == id), None)
+    post = get_post(id)
     if post is None:
         abort(404)
     return render_template("detail_post.html", post=post)
